4940f8801bd1bdc6922f6b7bb720b98d3b892f5b
Peter Lauri <peterlauri@gmail.com>
2016-01-05 19:45:48 +0100

    using {{ templatevar }} instead of {{templatevar}}

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "{{cookiecutter.project_name}}"
+echo "{{ cookiecutter.project_name }}"
--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -3,9 +3,9 @@
 virtualenv --version >/dev/null 2>&1 || { echo >&2 "virtualenv is required to use this cookiecutter"; exit 1; }
 
 virtualenv .ve
-.ve/bin/pip install django=={{cookiecutter.django_version}}
-.ve/bin/django-admin.py startproject "{{cookiecutter.project_name}}" .
-if [ "{{cookiecutter.keep_virtualenv}}" != "yes" ]; then
+.ve/bin/pip install django=={{ cookiecutter.django_version }}
+.ve/bin/django-admin.py startproject "{{ cookiecutter.project_name }}" .
+if [ "{{ cookiecutter.keep_virtualenv }}" != "yes" ]; then
     rm -rf .ve && echo "---> virtualenv .ve deleted"
 else
     echo "---> virtualenv created in .ve"
